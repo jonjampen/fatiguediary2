312760dcdbdf3bc407df8f56f3a18bd0b3ac7a91
Jon Jampen <jon.jampen@cryptography.ch>
2023-10-23 18:06:33 +0200

    Adding animations with framer motion.

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,43 @@
+"use client"
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import { Instagram, Mail, Youtube } from 'lucide-react'
+import { motion, stagger, useAnimate, animate } from "framer-motion"
+
 
 export default async function Home() {
+
+  const AnimatedButton = motion(Button)
 
   return (
     <main className="mx-24 min-h-screen">
       {/* Hero */}
-      <section className="flex w-full h-screen justify-between items-center">
+      <section className="flex w-full h-[80vh] justify-between items-center">
         <div className="max-w-2xl">
-          <h1 className="font-bold text-left text-5xl leading-tight">Track and Manage Fatigue with <span className="text-accent">Fatigue Diary</span></h1>
-          <h4 className="text-2xl">Effortlessly monitor, analyze, and improve your energy levels and daily activities with Fatigue Diary.</h4>
+          <motion.h1 className="font-bold text-left text-5xl leading-tight"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.4 }}>
+            Track and Manage Fatigue with&nbsp;
+            <span className="text-accent">
+              Fatigue Diary
+            </span>
+          </motion.h1>
+          <motion.h4 className="text-2xl"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}>
+            Effortlessly monitor, analyze, and improve your energy levels and daily activities with Fatigue Diary.
+          </motion.h4>
           <br />
-          <Button>Start now</Button>
+          <AnimatedButton
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.3 }}>
+            Start now
+          </AnimatedButton>
         </div>
         {/* <img src="/preview/app.svg" alt="Screenshot of the Fatigue Diary App" /> */}
         <h2>image</h2>
@@ -20,52 +45,81 @@ export default async function Home() {
 
       {/* Features */}
       <section>
-        <div className="flex justify-between h-screen items-center">
+        <div className="flex justify-between h-[80vh] items-center"
+        >
           {/* <img src="/preview/feature/track.svg" alt="Screenshot of how to track activities and energy levels in Fatigue Diary." /> */}
           <p>image</p>
-          <div className="max-w-md">
+          <motion.div className="max-w-md"
+            initial={{ opacity: 0, scale: 0.75 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}>
             <h2 className="font-semibold">Energy and Activity Tracking</h2>
             <p>Log energy level and activities after every activity which can help you a lot with pacing.</p>
-          </div>
+          </motion.div>
         </div>
-        <div className="flex justify-between h-screen items-center">
+        <div className="flex justify-between h-[80vh] items-center">
           {/* <img src="/preview/feature/analyze.svg" alt="Screenshot of how to analyze tracked data in Fatigue Diary." /> */}
           <p>image</p>
-          <div className="max-w-md">
+          <motion.div className="max-w-md"
+            initial={{ opacity: 0, scale: 0.75 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}>
             <h2 className="font-semibold">Gain Deeper Insights into Your Energy Levels</h2>
             <p>Visualize your energy trends through interactive charts to enhance your understanding of fatigue. Identify optimal times of the day and recognize potential patterns.</p>
-          </div>
+          </motion.div>
         </div>
-        <div className="flex justify-between h-screen items-center">
+        <div className="flex justify-between h-[80vh] items-center">
           {/* <img src="/preview/feature/auto-analysis.svg" alt="Screenshot of how Fatigue Diary automatically analyzes inputted data." /> */}
           <p>image</p>
-          <div className="max-w-md">
+          <motion.div className="max-w-md"
+            initial={{ opacity: 0, scale: 0.75 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}>
             <h2 className="font-semibold">Automatic Activity Analysis</h2>
             <p>Benefit from automated analysis to identify activities that promote relaxation and recovery, as well as uncover potential sources of energy depletion you may not be aware of.</p>
-          </div>
+          </motion.div>
         </div>
       </section>
 
       {/* Developer */}
       <section className="mb-28">
-        <div className="max-w-md">
+        <motion.div className="max-w-md"
+          initial={{ opacity: 0, scale: 0.75 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}>
           <h2 className="font-semibold">Developed by a fatigue patient</h2>
           <p>Hello, I'm Jon. Like you, I am facing chronic fatigue, and I've created this web app to share the strategies that have helped me on my journey. This project was initially my matura (high school graduation) project.</p>
           <p>Feel free to contact me if you have any questions!</p>
-        </div>
+        </motion.div>
         {/* <img src="" alt="" /> */}
       </section>
 
       {/* CTA */}
       <section className="border rounded min-h-[80vh] mb-28 flex items-center justify-between px-10 py-10">
-        <div className="max-w-md flex flex-col gap-8">
-          <h2 className="font-bold text-3xl">Use Fatigue Diary for FREE now!</h2>
-          <p className="text-xl">Effortlessly monitor, analyze, and improve your energy levels and daily activities with Fatigue Diary.</p>
+        <motion.div className="max-w-md flex flex-col gap-8"
+          initial={{ opacity: 0, scale: 0.75 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }}
+          animate={{ delay: stagger(0.3) }}
+        >
+          <motion.h2 className="font-bold text-3xl"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.7 }}>Use Fatigue Diary for FREE now!</motion.h2>
+          <motion.p className="text-xl"
+            initial={{ opacity: 0, scale: 0.75 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.7 }}>Effortlessly monitor, analyze, and improve your energy levels and daily activities with Fatigue Diary.</motion.p>
           <div className="flex gap-4">
             <Button>Start now</Button>
             <Button variant="accent">Instructions</Button>
           </div>
-        </div>
+        </motion.div>
         {/* <img src="" alt="" /> */}
       </section>
 
