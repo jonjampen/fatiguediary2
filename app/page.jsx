b667abd489c647bd91412ec28444717d7557f2a7
Jon Jampen <jon.jampen@cryptography.ch>
2023-10-24 16:33:30 +0200

    Adding scroll animation for features.

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,55 @@
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import { Instagram, Mail, Youtube } from 'lucide-react'
-import { motion, stagger, useAnimate, animate } from "framer-motion"
+import { motion, stagger, useScroll, useAnimation, useInView } from "framer-motion"
+import { useEffect, useRef } from 'react'
 
 
-export default async function Home() {
+export default function Home() {
 
   const AnimatedButton = motion(Button)
+
+  const featureOne = useRef(null)
+  const featureOneInView = useInView(featureOne)
+  let imageOneControl = useAnimation();
+
+  const featureTwo = useRef(null)
+  const featureTwoInView = useInView(featureTwo)
+  let imageTwoControl = useAnimation();
+
+  const featureThree = useRef(null)
+  const featureThreeInView = useInView(featureThree)
+  let imageThreeControl = useAnimation();
+
+  async function handleInView() {
+    if (featureOneInView) {
+      await imageOneControl.start({ opacity: 1 });
+      await imageTwoControl.start({ opacity: 0 });
+      await imageThreeControl.start({ opacity: 0 });
+    }
+    else if (featureTwoInView) {
+      await imageTwoControl.start({ opacity: 1 });
+      await imageOneControl.start({ opacity: 0 });
+      await imageThreeControl.start({ opacity: 0 });
+    }
+    else if (featureThreeInView) {
+      await imageThreeControl.start({ opacity: 1 });
+      await imageOneControl.start({ opacity: 0 });
+      await imageTwoControl.start({ opacity: 0 });
+      console.log("hey")
+    }
+  }
+
+  useEffect(() => {
+    console.log(featureThreeInView)
+    handleInView();
+  }, [featureOneInView, featureTwoInView, featureThreeInView])
 
   return (
     <main className="mx-24 min-h-screen">
       {/* Hero */}
-      <section className="flex w-full h-[80vh] justify-between items-center">
-        <div className="max-w-2xl">
+      <section className="flex w-full h-[90vh] justify-between items-center">
+        <div className="max-w-2xl" >
           <motion.h1 className="font-bold text-left text-5xl leading-tight"
             initial={{ opacity: 0, scale: 0.75 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -45,47 +82,37 @@ export default async function Home() {
 
       {/* Features */}
       <section>
-        <div className="flex justify-between h-[80vh] items-center"
-        >
-          {/* <img src="/preview/feature/track.svg" alt="Screenshot of how to track activities and energy levels in Fatigue Diary." /> */}
-          <p>image</p>
-          <motion.div className="max-w-md"
-            initial={{ opacity: 0, scale: 0.75 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.7 }}>
-            <h2 className="font-semibold">Energy and Activity Tracking</h2>
-            <p>Log energy level and activities after every activity which can help you a lot with pacing.</p>
-          </motion.div>
+        <div className="features flex justify-between items-start">
+          <div className="img h-screen w-1/2 sticky top-0">
+            <motion.img src="/preview/circle-screenshot.svg" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" animate={imageOneControl} initial={{ opacity: 1 }} transition={{ duration: 0 }} />
+            <motion.img src="/preview/circle-screenshot2.svg" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" animate={imageTwoControl} initial={{ opacity: 0 }} transition={{ duration: 0 }} />
+            <motion.img src="/preview/circle-screenshot3.svg" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" animate={imageThreeControl} initial={{ opacity: 0 }} transition={{ duration: 0 }} />
+          </div>
+          <div className="text h-[300vh] w-1/2" >
+            <div className="h-screen flex items-center">
+              <p ref={featureOne}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui placeat, pariatur iste aut voluptatum non quas necessitatibus consequuntur quia dolor vitae consequatur. Cupiditate dolores minus vitae eos et non, vel tenetur facere corrupti nemo odio molestias ullam voluptas quibusdam pariatur tempora ex voluptate ratione saepe fugiat. Cum repellat sint nam quaerat reprehenderit, provident sapiente dignissimos deserunt, explicabo quasi quidem maxime perferendis consequuntur est. A ut quas libero, ipsa cum, aliquid neque atque unde architecto voluptate minus beatae vitae? Nobis voluptatem error voluptatibus, ex sapiente excepturi perferendis ratione fugit odio nam libero debitis tempore rerum dolor facere soluta provident numquam?
+              </p>
+            </div>
+            <div className="h-screen flex items-center ">
+              <p ref={featureTwo}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui placeat, pariatur iste aut voluptatum non quas necessitatibus consequuntur quia dolor vitae consequatur. Cupiditate dolores minus vitae eos et non, vel tenetur facere corrupti nemo odio molestias ullam voluptas quibusdam pariatur tempora ex voluptate ratione saepe fugiat. Cum repellat sint nam quaerat reprehenderit, provident sapiente dignissimos deserunt, explicabo quasi quidem maxime perferendis consequuntur est. A ut quas libero, ipsa cum, aliquid neque atque unde architecto voluptate minus beatae vitae? Nobis voluptatem error voluptatibus, ex sapiente excepturi perferendis ratione fugit odio nam libero debitis tempore rerum dolor facere soluta provident numquam?
+              </p>
+            </div>
+            <div className="h-screen flex items-center ">
+              <p ref={featureThree}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui placeat, pariatur iste aut voluptatum non quas necessitatibus consequuntur quia dolor vitae consequatur. Cupiditate dolores minus vitae eos et non, vel tenetur facere corrupti nemo odio molestias ullam voluptas quibusdam pariatur tempora ex voluptate ratione saepe fugiat. Cum repellat sint nam quaerat reprehenderit, provident sapiente dignissimos deserunt, explicabo quasi quidem maxime perferendis consequuntur est. A ut quas libero, ipsa cum, aliquid neque atque unde architecto voluptate minus beatae vitae? Nobis voluptatem error voluptatibus, ex sapiente excepturi perferendis ratione fugit odio nam libero debitis tempore rerum dolor facere soluta provident numquam?
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-between h-[80vh] items-center">
-          {/* <img src="/preview/feature/analyze.svg" alt="Screenshot of how to analyze tracked data in Fatigue Diary." /> */}
-          <p>image</p>
-          <motion.div className="max-w-md"
-            initial={{ opacity: 0, scale: 0.75 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.7 }}>
-            <h2 className="font-semibold">Gain Deeper Insights into Your Energy Levels</h2>
-            <p>Visualize your energy trends through interactive charts to enhance your understanding of fatigue. Identify optimal times of the day and recognize potential patterns.</p>
-          </motion.div>
-        </div>
-        <div className="flex justify-between h-[80vh] items-center">
-          {/* <img src="/preview/feature/auto-analysis.svg" alt="Screenshot of how Fatigue Diary automatically analyzes inputted data." /> */}
-          <p>image</p>
-          <motion.div className="max-w-md"
-            initial={{ opacity: 0, scale: 0.75 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.7 }}>
-            <h2 className="font-semibold">Automatic Activity Analysis</h2>
-            <p>Benefit from automated analysis to identify activities that promote relaxation and recovery, as well as uncover potential sources of energy depletion you may not be aware of.</p>
-          </motion.div>
-        </div>
-      </section>
+
+
+
+      </section >
 
       {/* Developer */}
-      <section className="mb-28">
+      <section section className="mb-28" >
         <motion.div className="max-w-md"
           initial={{ opacity: 0, scale: 0.75 }}
           whileInView={{ opacity: 1, scale: 1 }}
@@ -96,10 +123,10 @@ export default async function Home() {
           <p>Feel free to contact me if you have any questions!</p>
         </motion.div>
         {/* <img src="" alt="" /> */}
-      </section>
+      </section >
 
       {/* CTA */}
-      <section className="border rounded min-h-[80vh] mb-28 flex items-center justify-between px-10 py-10">
+      <section section className="border rounded min-h-[80vh] mb-28 flex items-center justify-between px-10 py-10" >
         <motion.div className="max-w-md flex flex-col gap-8"
           initial={{ opacity: 0, scale: 0.75 }}
           whileInView={{ opacity: 1, scale: 1 }}
@@ -120,11 +147,15 @@ export default async function Home() {
             <Button variant="accent">Instructions</Button>
           </div>
         </motion.div>
-        {/* <img src="" alt="" /> */}
-      </section>
+        <motion.img src="/preview/circle-screenshot.svg" alt="Screenshot of Fatigue Diary"
+          initial={{ opacity: 0, scale: 0.75 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.7 }} />
+      </section >
 
       {/* Footer */}
-      <section className="flex items-start justify-between align-center">
+      <section section className="flex items-start justify-between align-center" >
         <div className="max-w-sm">
           <h3 className="font-bold text-2xl">Fatigue Diary</h3>
           <h4>Effortlessly monitor, analyze, and improve your energy levels and daily activities with Fatigue Diary.</h4>
@@ -154,10 +185,10 @@ export default async function Home() {
             </ul>
           </div>
         </div>
-      </section>
+      </section >
       <section className="w-full mt-8 pb-3">
         <p className="text-center">&copy;2023 All rights reserved. Designed and developed by <a href="https://jonjampen.ch" target="_blank" className="text-accent">Jon Jampen</a>.</p>
       </section>
-    </main>
+    </main >
   )
 }
