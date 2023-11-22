b173cfc92df7c27f1024b7440a695cc7ade92a88
Jon Jampen <jon.jampen@cryptography.ch>
2023-11-22 09:26:07 +0100

    Choosing language based on browser preference.

--- middleware.js
+++ middleware.js
@@ -1,21 +1,28 @@
 import { NextResponse } from 'next/server'
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
 import nextAuthMiddleware from "next-auth/middleware"
 
 let locales = ['en', 'de']
 let defaultLocale = 'en'
 
-function getLocale(request) {
-    let headers = { 'accept-language': 'en' }
-    // primarily set based on settings if provided
-    let languages = new Negotiator({ headers }).languages()
-    return match(languages, locales, defaultLocale) // -> 'en'
+function getLocale(acceptLanguage) {
+    const parsedLanguages = acceptLanguage
+        ? acceptLanguage.split(',').map(lang => lang.trim().split(';')[0].split('-')[0])
+        : [];
+
+    console.log(parsedLanguages)
+
+    // Use Intl to find the best match language
+    const selectedLocale = Intl.getCanonicalLocales(parsedLanguages)
+        .find(candidate => locales.includes(candidate)) || defaultLocale;
+
+    return selectedLocale;
+
 }
 
 export function middleware(request) {
     // cancel if exception
     const pathname = request.nextUrl.pathname
+    const { headers } = request
     const isException = ['/img', '/preview', '/icons', '/logo.svg', '/api', '/manifest.json', '/sendEmail.php', '/sendGeneralEmail.php', '/sw.js'].some((allowedPath) =>
         pathname.startsWith(`${allowedPath}`),
     );
@@ -28,7 +35,8 @@ export function middleware(request) {
 
     // Redirect if there is no locale
     if (pathnameIsMissingLocale) {
-        const locale = getLocale(request)
+        console.log("al", headers.get('accept-language'))
+        const locale = getLocale(headers.get('accept-language'))
         return NextResponse.redirect(
             new URL(`/${locale}/${pathname}`, request.url)
         )
